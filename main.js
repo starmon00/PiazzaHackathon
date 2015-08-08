99e72b470bac231dba2abfa2a9a59439079ec096
starmon00 <vcphng@hotmail.com>
2015-08-08 14:02:31 -0700

    temp
    pause

--- main.js
+++ main.js
@@ -2,12 +2,35 @@ function run() {
 	var json = $.getJSON('http://a.ashwinikhare.in:6060/getCourses', function(data){
 		if (window.console) console.log(data);		
 		
+
+		//Create Table
+		var table = $('<table>');
+
+
+
+
+
 		$.each(data, function( index, value ) {
-			
+			//Create Row
+
+			//Fill Row with Data
+			//Append to Table
 			if (window.console) console.log(value['estimatedClassWorkload']);		
 			
-		  	var div = $("<div>" +  "<img src='" + value['photo'] + "'>' <br>" + value['name'] + "<br> " + value['estimatedClassWorkload'] + "<br> " + value['service'] + " <br> " + value['shortName'] + " <br> " + value['shortDescription'] + "<br><br></div>");
+		  	var div = $("<div>" +  "<img src='" + value['photo'] + "'>' <br>" + value['name'] + "<br> " + value['estimatedClassWorkload'] 
+		  		+ "<br> " + value['service'] + " <br> " + value['shortName'] + " <br> " + value['shortDescription'] + "<br><br></div>");
+
+			var photo = $("<img src='" + value['photo'] + "'>'")
+			var name = $(value['name'] + "<br>")
+			var estimatedClassWorkload = $(value['estimatedClassWorkload'])
+			var service = $(value['service'])
+			var shortName = $(value['shortName'])
+			var shortDescription = $(value['shortDescription'])
+
+
+
 			$("body").append(div);
+
 		
 		});
 		
