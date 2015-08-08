7ed076cf2b1398c7c7e9536eef25df4bb9217996
Adela <a.bar.of.soap@gmail.com>
2015-08-08 16:53:14 -0700

    configure search to use enter

--- js/index.js
+++ js/index.js
@@ -2,19 +2,30 @@ function inputValue (newValue) {
 	document.getElementById("range").innerHTML=newValue;
 } 
 
-
 function submit() {
+	var key = $("#keyword").val();
 
-	$('#submit').click(function() {
-		var key = $("#keyword").val();
-		
-		if (key == '') {
-			alert('Please enter a keyword to search for!');
-		} else {
-			var level = $("#level option:selected" ).text();
-			var time = $("#range").text();
-			window.location = 'searchResult.html?key=' + key +  '&level=' + $.trim(level)  + '&time=' + time;	
-		}	
-	});
-	
+	if (key == '') {
+		alert('Please enter a keyword to search for!');
+	} else {
+		var level = $("#level option:selected" ).text();
+		var time = $("#range").text();
+		window.location = 'searchResult.html?key=' + key +  '&level=' + $.trim(level)  + '&time=' + time;	
+	}	
 }
+
+
+function bindSubmit() {
+	$('#keyword').keypress(function (e) {
+	  if (e.which == 13) {
+	    submit();
+	    return false;    //<---- Add this line
+	  }
+	});
+
+	$('#submit').click(submit);
+
+}
+
+
+
